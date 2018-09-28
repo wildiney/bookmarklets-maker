f5fb85b2d250cbd45481cd8c7e2dbb37a968358c
Fernando Di Masi <wildiney@gmail.com>
2018-09-28 17:43:20 -0300

    downgrade gulp to 3.9.x

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
-"use strict";
+"use strict"
 
 var gulp = require("gulp");
 var bookmarklet = require('gulp-bookmarklet');
 
-gulp.task('bookmarklet', function() {
+gulp.task('bookmark', function() {
     return gulp.src('src/*.js')
         .pipe(bookmarklet())
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('watch',['bookmarklet'],function(){
-    gulp.watch("./src/*.js",['bookmarklet']);
-});
+gulp.task('watch', function(){
+    gulp.watch('src/*.js',['bookmark']);
+})
 
-gulp.task('default',['bookmarklet','watch']);
+gulp.task('default',['bookmark', 'watch']);
--- src/bookmarklet-gettext.js
+++
@@ -1,8 +0,0 @@
-function getPageText(){
-    var script=document.createElement('script');
-    script.setAttribute("id","gettext");
-    script.setAttribute("type", "text/javascript");
-    script.setAttribute("src","https://www.slicedpixel.com/apps/save-page-text-to-doc/gettext.js"); 
-    document.getElementsByTagName('head')[0].appendChild(script);
-}
-getPageText();
